c15037106d98df9595ca58aed52833ae2778bfb0
hkdywg <hkdywg@163.com>
2025-02-11 11:15:17 +0800

    Update: top level build script, support project select and compile tool chain configure
    Signed-off-by: hkdywg <hkdywg@163.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 TOOLCHAIN_URL="https://releases.linaro.org/components/toolchain/binaries/7.4-2019.02/aarch64-linux-gnu/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu.tar.xz"
+BUILD_PROJECT=
+BUILD_PROJECT_CONFIG=
+PROJECT_ARRAY=
 
 function print_info()
 {
@@ -10,6 +13,18 @@ function print_info()
 function print_error()
 {
 	printf "\e[1;31mError: %s\e[0m\n" "$1"
+}
+
+function build_usage()
+{
+	echo "Usage: "
+	echo ""$0"          - Show this menu"
+	echo ""$0" lunch        - Select a project to update"
+	echo ""$0" [project]    - build [project] directly, support project as  follows:"
+
+	for project in "${PROJECT_ARRAY}"; do
+		print_info "$project"
+	done
 }
 
 function get_toolchain()
@@ -45,4 +60,92 @@ function get_toolchain()
 }
 
 
-get_toolchain
+function get_available_project()
+{
+    PROJECT_ARRAY=$(find device -mindepth 1 -maxdepth 1 -not -path "device/common" -type d -print ! -name "." | awk -F/ '{print $NF}' | sort -t '-' -k2,2)
+    #print_info "${PROJECT_ARRAY}"
+    local len
+    len=$(echo $PROJECT_ARRAY | tr -cd ' ' | wc -c)
+    PROJECT_ARRAY_LEN=$(($len+1))
+	if [ ${PROJECT_ARRAY_LEN} -eq 0 ];then
+		print_error "No available config board"
+		exit 1
+	fi
+}
+
+function choose_project()
+{
+    echo "Select a target project"
+
+    echo ${PROJECT_ARRAY} | xargs -n 1 | sed "=" | sed "N;s/\n/. /"
+
+    local index
+    read -p "Which would you select: " index
+
+    if [ -z $index ]; then
+    print_err "Nothing select"
+    exit 0
+    fi
+
+    if [[ -n $index && $index =~ ^[0-9]+$ && $index -ge 1 && $index -le $PROJECT_ARRAY_LEN ]]; then
+    #set -- $PROJECT_ARRAY
+    BUILD_PROJECT=$(echo $PROJECT_ARRAY | awk -v field="$index" '{print $field}')
+    else
+    print_err "Invalid input!"
+    exit 1
+    fi
+}
+
+function prepare_env()
+{
+	BUILD_PROJECT_CONFIG=device/${BUILD_PROJECT}/project_config.sh
+	if [ ! -f ${BUILD_PROJECT_CONFIG} ]; then
+		print_error "${BUILD_PROJECT_CONFIG} not found!"
+		exit 1
+	fi
+
+	source ${BUILD_PROJECT_CONFIG}
+	source build/${BUILD_ENV} > /dev/null 2>&1
+
+	print_info "OUTPUT_DIR: ${OUTPUT_DIR}"
+}
+
+function project_build() 
+{
+	build_all
+	if [ $? -rq 0 ];then
+		print_info "Build project ${BUILD_PROJECT} success!"
+	else
+		print_error "Build project  ${BUILD_PROJECT} failed!"
+		exit 1
+	fi
+}
+
+
+#get_toolchain
+get_available_project
+if [  $# -eq 1 ]; then
+    if [ "$1" = "lunch" ]; then
+    choose_project || exit 0
+    else
+    target=$1
+    found=0
+    #set -- $PROJECT_ARRAY
+    for word in $PROJECT_ARRAY; do
+        if [ "$word" = "$target" ]; then
+        BUILD_PROJECT=$target
+        found=1
+        break
+        fi
+    done
+    if [ $found -eq 0 ]; then
+        print_err "input project is invalid, not support yet!"
+        exit 1
+    fi
+    fi
+else
+    build_usage && exit 0
+fi
+prepare_env
+project_build
+
