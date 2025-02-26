70c5bc63badf321cc8126dc2a59b6517839f0344
hkdywg <hkdywg@163.com>
2025-02-26 17:54:33 +0800

    Update: support boot image and ramdisk file system compile
    Signed-off-by: hkdywg <hkdywg@163.com>

--- build/build_core.sh
+++ build/build_core.sh
@@ -1,50 +1,61 @@
 #!/bin/bash
 
+# partition generation:
+# pack_boot -> boot.emmc / boot.spinand (kernel+dtb)
+# pack_rootfs -> rootfs.emmc / rootfs.spinand ( rootfs )
+# pack_system -> system.emmc / system.spinand (3rd/sdk shared libraries,  spinand-ubifs/ emmc-ext4)
+# pack_gpt -> gpt.img (emmc only)
+# pack_cfg -> cfg.emmc / cfg.spinand (configs partition for saving configs)
+
 function path_remove()
 {
-	local IFS=':'						# 设置内部字段分隔符为冒号(:)
-	local newpath dir 					# 临时变量，用于遍历及存储
-	local path_variable=${2:-PATH}		# 如果第二个参数没有传递，则默认为PATH
+	local IFS=':'						
+	local newpath dir 					
+	local path_variable=${2:-PATH}		
 
-	# 遍历路径变量中的每个目录
 	for dir in ${!path_variable};  do
-		if [ "$dir" != "$1" ]; then		# 如果当前目录不等于要删除的目录
-			newpath=${newpath:+$newpath:}$dir	#将当前目录添加到newpath中
+		if [ "$dir" != "$1" ]; then	
+			newpath=${newpath:+$newpath:}$dir
 		fi
 	done
-	# 设置PATH为修改后的路径
+
 	export $path_variable="$newpath"
 }
 
 function path_prepend()
 {
 	path_remove $1 $2
-	local path_variable=${2:-PATH}		# 如果第二个参数没有传递，则默认为PATH
+	local path_variable=${2:-PATH}
 	export $path_variable="$1${!path_variable:+:${!path_variable}}"
 }
 
 function path_pend()
 {
 	path_remove $1 $2
-	local path_variable=${2:-PATH}		# 如果第二个参数没有传递，则默认为PATH
+	local path_variable=${2:-PATH}
 	export $path_variable="${!path_variable:+${!path_variable}:}$1"
 }
 
-# linux 内核编译
+
 function build_kernel()
 {
 	cd ${BUILD_PATH} || return
 	make kernel || return $?
 }
 
-# rootfs 编译
 function build_rootfs()
 {
 	cd ${BUILD_PATH} || return
 	make rootfs || return $?
 }
 
-# 所有子项编译入口
+function build_boot()
+{
+	echo "--------------"
+	cd ${BUILD_PATH} || return
+	make boot || return $?
+}
+
 function build_all()
 {
 	# build bsp
